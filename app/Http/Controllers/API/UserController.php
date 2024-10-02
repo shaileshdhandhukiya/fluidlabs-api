1e16f1a315f66b8b657cd3297555a6fd0a0de432
shaileshdhandhukiya <shaileshdhandhukiya012@gmail.com>
2024-10-02 17:19:14 +0530

    profile image field make not required

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -45,7 +45,7 @@ class UserController extends BaseController
         $validator = Validator::make($request->all(), [
             'first_name' => 'required|string',
             'last_name' => 'required|string',
-            'profile_photo' => 'required|image|mimes:jpeg,png,jpg,gif',
+            'profile_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif',
             'type' => 'nullable|string',
             'phone' => 'nullable|string',
             'date_of_birth' => 'nullable|date',
@@ -135,7 +135,7 @@ class UserController extends BaseController
         $validator = Validator::make($request->all(), [
             'first_name' => 'required|string',
             'last_name' => 'required|string',
-            'profile_photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'profile_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'type' => 'nullable|string',
             'phone' => 'nullable|string',
             'date_of_birth' => 'nullable|date',
